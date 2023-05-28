a48c108dc98347f404e54820d402a74df1ff5247
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2023-05-28 23:21:17 +0530

    Update PersonRepository.java

--- src/main/java/com/dailycodebuffer/springbootmongodb/repository/PersonRepository.java
+++ src/main/java/com/dailycodebuffer/springbootmongodb/repository/PersonRepository.java
@@ -4,7 +4,6 @@ import com.dailycodebuffer.springbootmongodb.collection.Person;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
