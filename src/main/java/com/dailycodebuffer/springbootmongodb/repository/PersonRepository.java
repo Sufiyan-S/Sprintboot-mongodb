87c6283cffc15d2776faf3ef2f872e8fd62baeff
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2023-05-28 23:21:23 +0530

    Update PersonRepository.java

--- src/main/java/com/dailycodebuffer/springbootmongodb/repository/PersonRepository.java
+++ src/main/java/com/dailycodebuffer/springbootmongodb/repository/PersonRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
+
+
+
 @Repository
 public interface PersonRepository extends MongoRepository<Person,String> {
 
