b0188cc6ee17a5d4b7e7c1ed60ee9d149d2bcf88
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2023-05-31 00:02:26 +0530

    Update PhotoRepository.java

--- src/main/java/com/dailycodebuffer/springbootmongodb/repository/PhotoRepository.java
+++ src/main/java/com/dailycodebuffer/springbootmongodb/repository/PhotoRepository.java
@@ -4,7 +4,6 @@ import com.dailycodebuffer.springbootmongodb.collection.Photo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface PhotoRepository extends MongoRepository<Photo, String> {
 }
