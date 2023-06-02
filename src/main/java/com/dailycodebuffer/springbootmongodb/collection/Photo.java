c1904e168272c2f62fef2ff75cbd41e6f10cbbcb
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2023-06-03 00:01:23 +0530

    Update Photo.java

--- src/main/java/com/dailycodebuffer/springbootmongodb/collection/Photo.java
+++ src/main/java/com/dailycodebuffer/springbootmongodb/collection/Photo.java
@@ -7,7 +7,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
-
 @Data
 @Document(collection = "photo")
 @JsonInclude(JsonInclude.Include.NON_NULL)
