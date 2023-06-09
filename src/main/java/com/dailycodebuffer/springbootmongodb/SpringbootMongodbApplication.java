8ae007d1f8b1e3cd242bf2e88a930b5480756e02
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2023-06-10 00:20:27 +0530

    Update SpringbootMongodbApplication.java

--- src/main/java/com/dailycodebuffer/springbootmongodb/SpringbootMongodbApplication.java
+++ src/main/java/com/dailycodebuffer/springbootmongodb/SpringbootMongodbApplication.java
@@ -3,6 +3,7 @@ package com.dailycodebuffer.springbootmongodb;
 
 
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
