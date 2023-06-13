61abf45b053a9487b199b6382059c0a96a178261
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2023-06-13 22:48:18 +0530

    Update SpringFoxConfig.java

--- src/main/java/com/dailycodebuffer/springbootmongodb/config/SpringFoxConfig.java
+++ src/main/java/com/dailycodebuffer/springbootmongodb/config/SpringFoxConfig.java
@@ -7,6 +7,15 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+
+
+
+
+
+
+
+
+
 @Configuration
 public class SpringFoxConfig {
 
