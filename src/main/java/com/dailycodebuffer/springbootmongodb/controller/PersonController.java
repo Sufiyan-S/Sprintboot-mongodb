cacbc099822a5982b822f0b91e0437575bc0bc1e
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2023-07-25 23:08:28 +0530

    Update PersonController.java

--- src/main/java/com/dailycodebuffer/springbootmongodb/controller/PersonController.java
+++ src/main/java/com/dailycodebuffer/springbootmongodb/controller/PersonController.java
@@ -4,7 +4,6 @@ package com.dailycodebuffer.springbootmongodb.controller;
 
 
 
-
 import com.dailycodebuffer.springbootmongodb.collection.Person;
 import com.dailycodebuffer.springbootmongodb.service.PersonService;
 import org.bson.Document;
