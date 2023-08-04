eff19f7b096c9c513d64f131ba59e353de2afd81
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2023-08-04 22:04:06 +0530

    Update PhotoController.java

--- src/main/java/com/dailycodebuffer/springbootmongodb/controller/PhotoController.java
+++ src/main/java/com/dailycodebuffer/springbootmongodb/controller/PhotoController.java
@@ -1,5 +1,6 @@
 package com.dailycodebuffer.springbootmongodb.controller;
 
+
 import com.dailycodebuffer.springbootmongodb.collection.Photo;
 import com.dailycodebuffer.springbootmongodb.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
