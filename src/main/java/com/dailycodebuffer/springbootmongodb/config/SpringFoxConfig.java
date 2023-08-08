e05a700ff1060976e33fc42b85f349c003b5abc1
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2023-08-08 23:51:05 +0530

    Update SpringFoxConfig.java

--- src/main/java/com/dailycodebuffer/springbootmongodb/config/SpringFoxConfig.java
+++ src/main/java/com/dailycodebuffer/springbootmongodb/config/SpringFoxConfig.java
@@ -15,7 +15,6 @@
 
 package com.dailycodebuffer.springbootmongodb.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
